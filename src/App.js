b469c64dd89f322eeb8caa077c10ce0e1e67029f
Arnas Čižikovas <arnas.cizikovas@altibox.no>
2023-09-27 12:13:01 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import {Header} from './components/header/header';
+import {Container} from "react-bootstrap";
 
 function App() {
     return (
-        <div className="container">
-            <Header />
-            <main className="container">
-                <div>
-                    need to test this first
-                </div>
-            </main>
+        <div>
+            div works!
+            <div>
+                Here should be header
+                <Header />
+            </div>
+            bellow is a container div
+            <div className="container">
+                container
+            </div>
+            <Container>
+                And this is react container
+            </Container>
         </div>
     );
 }
