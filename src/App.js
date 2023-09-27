9718ddba5e5d4f54f7bad54955d1552559be37f8
Arnas Čižikovas <arnas.cizikovas@altibox.no>
2023-09-27 12:09:08 +0300

    test

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {Header} from './components/header/header';
 function App() {
     return (
         <div className="container">
-            <Header/>
-            <main className="content">
+            <Header />
+            <main className="container">
                 <div>
                     need to test this first
                 </div>
