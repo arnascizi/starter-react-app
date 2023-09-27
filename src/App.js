47441195744bed77b6fa4489f49ad5283bd65141
Arnas Čižikovas <arnas.cizikovas@altibox.no>
2023-09-27 12:02:26 +0300

    Add header import

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Header } from './components/header/header';
 
 function App() {
   return (
-      <div className="container">
-        <Header />
-        <main className="content">
-          need to test this first
-        </main>
-      </div>
+      // <div className="container">
+      //   <Header />
+      //   <main className="content">
+            <div>
+                need to test this first
+            </div>
+        // </main>
+      // </div>
   );
 }
 
